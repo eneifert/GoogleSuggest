03c6d31d33ed616d8866fee2d166f43c965cd3e9
Eric Neifert <eric.neifert@gmail.com>
2010-12-30 12:41:15 -0500

    first commit

---
+++ GoogleSuggest/GoogleSuggest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+
+namespace GoogleSuggest
+{
+    public class GoogleSuggest
+    {
+        public static string GetSuggestion(string text)
+        {
+            return GetSuggestions(text)[0].suggestion;            
+        }
+
+        public static List<CompleteSuggestion> GetSuggestions(string text)
+        {
+            RestClient r = new RestClient("http://google.com/complete/search");
+            RestRequest req = new RestRequest();
+            req.AddParameter("q", text);
+            req.AddParameter("output", "toolbar");
+            req.RequestFormat = DataFormat.Xml;
+            r.AddHandler("text/xml", new GoogleXmlDeserializer());
+
+            return r.Execute<List<CompleteSuggestion>>(req).Data;   
+        }        
+    }
+
+    public class CompleteSuggestion
+    {
+        public string suggestion { get; set; }
+        public string num_queries { get; set; }
+    }  
+}
---
+++ GoogleSuggest/GoogleSuggestTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GoogleSuggest
+{
+    [TestFixture]
+    class GoogleSuggestTests
+    {
+        private string _testString = "miccrosoft";
+        private string _hopedForResult = "microsoft";
+
+        [Test]
+        public void GetSuggestion_Works()
+        {
+            Assert.AreEqual(_hopedForResult, GoogleSuggest.GetSuggestion(_testString));
+        }
+
+        [Test]
+        public void GetSuggestions_Works()
+        {
+            List<CompleteSuggestion> sugs = GoogleSuggest.GetSuggestions(_testString);
+
+            Assert.Greater(sugs.Count, 0);
+            foreach (var s in sugs.Take(5))
+            {
+                Assert.AreNotEqual("", s.suggestion);
+            }
+
+        }
+    }
+}
